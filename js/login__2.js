3d5f356d267ddfcfe877a78bfd7c6b1b36047316
adavice <97439259+adavice@users.noreply.github.com>
2022-11-19 23:20:15 +0200

    Add files via upload

---
+++ js/login__2.js
@@ -0,0 +1,19 @@
+// If logged in:
+// TODO: Insert a correct values here:
+
+var user = {
+	number: 38050576430155,
+	isAuthorized: false,
+	credits: 0,
+	score: ,
+}
+
+
+// Cheching of login/profile window
+
+
+// if there is a calc check
+
+		if (user.isAuthorized == true) {
+			document.querySelector('.login__alternate').style.display = 'none';
+			calc_window.style.display = 'block'; } 
